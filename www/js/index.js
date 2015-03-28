68fc7a5554b4cb7a01f339bac6bed27a04db8f36
drath1 <chingoodi@gmail.com>
2015-03-28 02:12:05 +0000

    Adding all animals

--- www/js/index.js
+++ www/js/index.js
@@ -21,7 +21,33 @@ var app = {
     // var currxPage = 0;
 
     currPage: 0,
-    page_ids: ["Page_01", "Page_02"],
+    page_ids: [ "Page_01", 
+                "Page_02",
+                "Page_03",
+                "Page_04",
+                "Page_05",
+                "Page_06",
+                "Page_07",
+                "Page_08",
+                "Page_09",
+                "Page_10",
+                "Page_11",
+                "Page_12",
+                "Page_13",
+                "Page_14",
+                "Page_15",
+                "Page_16",
+                "Page_17",
+                "Page_18",
+                "Page_19",
+                "Page_20",
+                "Page_21",
+                "Page_22",
+                "Page_23",
+                "Page_24",
+                "Page_25",
+                "Page_26",
+                ],
 
     // Application Constructor
     initialize: function() {
@@ -32,7 +58,12 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        //This is needed so that we can debug in chrome in cloud
+        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+            document.addEventListener("deviceready", this.onDeviceReady, false);
+        } else {
+            app.onDeviceReady();
+        }
     },
     // deviceready Event Handler
     //
@@ -56,6 +87,7 @@ var app = {
     // Start function
     start: function() {
         //animalId = $.mobile.activePage.attr('id');
+        $.mobile.defaultPageTransition = 'slide';
         app.registerPageSwipeHandlers();
     },
     // Change page on swipe
@@ -66,12 +98,20 @@ var app = {
                 // alert(app.currPage);
                 // app.currPage = app.currPage + 1;
                 ++app.currPage;
-                $("#" + this.id).attr("id", app.page_ids[app.currPage]);
+                if (app.currPage < app.page_ids.length) {
+                    $("#" + this.id).attr("id", app.page_ids[app.currPage]);
+                } else {
+                    alert("Last page");
+                }
             });
 
             $("#" + this.id).swiperight(function() {
                 --app.currPage;
-                $("#" + this.id).attr("id", app.page_ids[app.currPage]);
+                if (app.currPage >= 0) {
+                    $("#" + this.id).attr("id", app.page_ids[app.currPage]);   
+                } else {
+                    alert("First page");
+                }
             });
         });
     }
