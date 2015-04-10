581faef9d556153dcb9d2205505360aa4ffabb34
Devendra <devendra_rath@yahoo.com>
2015-04-10 23:33:43 +0530

    removing the plaform dir from repo

--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/devendra/work/phonegap_projects/animal_letters/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/devendra/work/phonegap_projects/animal_letters/platforms/android/ant-build/AndroidManifest.xml : \
-/home/devendra/work/phonegap_projects/animal_letters/platforms/android/AndroidManifest.xml \
